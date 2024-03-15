26cf0ee4083ed016c71795d069ef47de8163d795
yaraoyoussef <118832851+yaraoyoussef@users.noreply.github.com>
2024-03-15 11:01:16 +0100

    completion of function
    readDataset function update

--- test-generator.cpp
+++ test-generator.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <list>
 using namespace std;
 
-bool readDataset() {
-    // Read dataset
+bool readDataset(const string& filename, list<string>& lines) {
+
+    ifstream file(filename);
+    // check for errors while opening file
+    if (!file.is_open()) {
+        cerr << "Error: Unable to open file " << filename << endl;
+        return false;
+    }
+
+    // read first line of db to define all features
+    string line;
+    if (getline(file, line)) {
+        lines.push_back(line);
+    }
+
+
+    // close file and return 
+    file.close();
     return true;
 }
 
@@ -12,6 +31,9 @@ bool loadDataset() {
 }
 
 int main(int argc, char **argv) {
+
+    list<string> lines;
+    readDataset("dataset/dataset_clean.csv", lines);
     
     return 0;
 }
