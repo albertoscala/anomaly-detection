66297a23349f227017bcc36a029f1ef3a708eedb
yaraoyoussef <118832851+yaraoyoussef@users.noreply.github.com>
2024-03-15 12:05:28 +0100

    load dataset
    load dataset to redis function completed

--- test-generator.cpp
+++ test-generator.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <sstream>
 #include <list>
+#include <hiredis/hiredis.h>
+
 using namespace std;
 
 bool readDataset(const string& filename, list<string>& lines) {
@@ -25,8 +27,44 @@ bool readDataset(const string& filename, list<string>& lines) {
     return true;
 }
 
-bool loadDataset() {
-    // Load dataset
+// Function that handles connection to Redis server
+redisContext* connectToRedis(const char* hostname, int port) {
+
+    redisContext* context = redisConnect(hostname, port);
+
+    // failed to connect
+    if (context == NULL || context->err) {
+        if (context) {
+            cerr << "Connection error: " << context->errstr << endl;
+            redisFree(context);
+        } else {
+            cerr << "Error: Failed to allocate Redis context" << endl;
+        }
+        return nullptr;
+    }
+
+    // connection successful 
+    cout << "Connected to Redis server" << endl;
+    return context;
+}
+
+bool loadDataset(const char* hostname, int port, const list<string>& lines) {
+    // connect to Redis server
+    redisContext* context = connectToRedis(hostname, port);
+
+    int line_numb = 1;
+    for(const auto& line : lines) {
+        redisReply* reply = (redisReply*)redisCommand(context, "SET %d %s", line_numb, line.c_str());
+        if (!reply) {
+            cerr << "Failed to execute Redis command" << endl;
+            redisFree(context);
+            return false;
+        }
+        freeReplyObject(reply);
+        line_numb++;
+    }
+
+    redisFree(context);
     return true;
 }
 
@@ -34,6 +72,7 @@ int main(int argc, char **argv) {
 
     list<string> lines;
     readDataset("dataset/dataset_clean.csv", lines);
+    loadDataset("localhost", 6379, lines);
     
     return 0;
 }
